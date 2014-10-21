abc79405b13b53df35168fb9a249f9b65326795e
vladik7go <ivanfil2@rambler.ru>
2014-10-21 17:47:15 +0300

    home junit

---
+++ test.by.epam.project/TestConnectionPool.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import by.epam.project.exception.TechnicalException;
+import by.epam.project.pool.ConnectionPool;
+
+
+public class TestConnectionPool {
+
+	@Test
+	public void GetConnectionTest() throws TechnicalException {
+		ConnectionPool pool = ConnectionPool.getSinglePool();
+		Connection connection = pool.getConnection();
+		Assert.assertNotNull(connection);
+	}
+
+}
